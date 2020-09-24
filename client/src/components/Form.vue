a4489c6fa810f3b2e9d80b4983fb08cc1e8be435
Florent <flgallois@gmail.com>
2020-09-24 15:01:39 +0200

    FEATURE: CHANGE WITH ELEMENT UI
    CREATE LOAD IN CASE OF LOADING

--- client/src/components/Form.vue
+++ client/src/components/Form.vue
@@ -1,45 +1,39 @@
 <template>
   <div>
     <h1> Convertion en nombre romain</h1>
-    <form
-      ref="number"
-      name="number"
-      :model="number" 
-       @submit="checkInput"
-      enctype="multipart/form-data"
-      onsubmit="return false;"
-      novalidate="true"
-    >
-      <p class="error">
-        {{ error }}
-      </p>
-     <p>
-        <label for="decimal" style="width:100px;margin-right:2px;">Nombre entre 0 et 100</label>
-        <input
-          id="decimal"
-          v-model="number.decimal"
-          type="number"
-          name="decimal"
-          style="margin-right:2px;"
-          >
-
-      </p>
-      <p>
-        <label for="decimal" style="width:100px;margin-right:2px;"> <img style="height: 20px;" src="./../assets/roman-helmet.svg"></label>
-         <input
-          v-model="romanoNumber"
-          type="string"
-          name="romanoNumber"
-          style="margin-right:2px;"
-          disabled
-          >
-<!--          
-          <span>{{romanoNumber}}</span> -->
-     </p>
-     <p>
-               <button class="pointer">Convertir</button>
-     </p>
-    </form>
+    <el-row style="margin-top: 10px;">
+      <el-col
+      :span="10"
+      :offset="7"
+      v-loading="loading">
+      <el-form 
+        ref="form" 
+        :model="number"
+        label-position="left"
+        label-width="200px">
+         <el-row style="height:20px;margin-bottom:5px;color:red">{{ error }}</el-row>
+          <el-form-item label="Nombre entre 0 et 100">
+            <el-input
+              id="decimal"
+              v-model="number.decimal"
+              name="decimal"
+              size="mini"
+              type="number">
+            </el-input>
+          </el-form-item>
+          <el-form-item label="Romano number">
+             <el-input
+              v-model="romanoNumber"
+              type="string"
+              name="romanoNumber"
+              size="mini"
+              disabled>
+              </el-input>
+          </el-form-item>
+          <el-button type="primary" plain size="mini" @click="checkInput">Convertir</el-button>
+        </el-form>
+    </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -56,49 +50,43 @@ export default {
       },
       romanoNumber: '',
       error: '',
-      isLoading: false,
+      loading: false,
       isError: false,
     }
   },
   methods: {
-    convert() {
-      this.$refs['number'].validate((valid) => {
-        console.log(valid);
-    });
-    },
-    checkInput (e) {
+    checkInput () {
+      this.loading = true;
       let nb = parseInt(this.number.decimal);
       if (nb === '' || isNaN(nb)) {
         console.log('isnotanumber');
          this.error = "Veuillez entrer un nombre svp";
+         this.loading = false;
       }
       else if (typeof nb !== "number") {
         console.log('is not a number');
-        this.error = "Veuillez entrer un nombre svp"
+        this.error = "Veuillez entrer un nombre svp";
+          this.loading = false;
       } else if (nb < 0 || nb > 100) {
          console.log('is not between 0 and 100');
-        this.error = "Entrez un nombre entre 0 et 100 svp"
+        this.error = "Entrez un nombre entre 0 et 100 svp";
+        this.loading = false;
       } else {
         console.log('Is a Number between 0 and 100')
-        this.error = ''
+        this.error = '';
+        this.loading = false;
         this.convertNumber(nb);
       }
-
-      e.preventDefault();
-
     },
     async convertNumber (nb) {
       console.log('convert Function');
       console.log(nb);
-      //  NumberService.getNumber().then(function (value) {
-      //   console.log('Retour value');
-      //   console.log(value);
-      // }).catch();
       let context = this;
       NumberService.convertNumber(nb).then(function (value) {
         console.log('Retour value');
         console.log(value);
         context.romanoNumber = value.romanoNumber;
+        context.loading = false;
       }).catch();
 
     }
--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+import 'element-ui/lib/theme-chalk/index.css';
+import "./style/element-variables.scss";
 
 Vue.config.productionTip = false
 Vue.use(axios);
-
+Vue.use(ElementUI, { locale });
 
 new Vue({
   render: h => h(App),
