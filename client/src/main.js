35e7c3849879b806ddaa9575a93b8b22df48b393
Florent <flgallois@gmail.com>
2020-09-25 23:47:17 +0200

    CORRECTION: REMOVE COMMENTS

--- client/src/main.js
+++ client/src/main.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import { Form, FormItem, Button, Input, Col, Row, Loading } from 'element-ui'
-// import locale from 'element-ui/lib/locale/lang/en'
-// import 'element-ui/lib/theme-chalk/index.css';
 import "./style/element-variables.scss";
 
 Vue.config.productionTip = false
@@ -17,7 +15,6 @@ Vue.use(Row)
 Vue.use(Loading)
 
 Vue.use(axios);
-// Vue.use(ElementUI, { locale });
 
 new Vue({
   render: h => h(App),
